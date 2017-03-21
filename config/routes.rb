8c6db8b19c84f18d5af0ae4b75c5c57085563863
Mark Miranda <notmarkmiranda@gmail.com>
2017-03-20 21:17:45 -0600

    closes #3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "pages#home"
+
+  get "/not-found", to: "pages#not_found", as: "not_found"
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe PagesController, type: :controller do
     get :home
     expect(response).to render_template(:home)
   end
+
+  it "Get not_found - template" do
+    get :not_found
+    expect(response).to render_template(:not_found)
+  end
 end
