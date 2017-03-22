b409d88137957dec43be8bdbbb88c88f58b50ec6
Mark Miranda <notmarkmiranda@gmail.com>
2017-03-22 08:17:47 -0600

    finishes tests for log in and log out

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root "pages#home"
   resources :users, only: [:index, :create]
 
+  get "/login", to: "sessions#new", as: "login"
+  post "/login", to: "sessions#create"
+  get "/logout", to: "sessions#destroy", as: "logout"
   get "/dashboard", to: "users#show", as: "dashboard"
   get "/new-account", to: "users#new", as: "new_user"
   get "/not-found", to: "pages#not_found", as: "not_found"
