1aa0e86f68baf8780277335649b18ddde9482296
Mark Miranda <notmarkmiranda@gmail.com>
2017-03-24 16:49:32 -0600

    closes #23

--- spec/features/leagues/user_can_edit_league_name_spec.rb
+++ spec/features/leagues/user_can_edit_league_name_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "User can edit a league name" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
   end
 
-  it "edits the leagues name and slug - from dashboard (index)" do
+  it "edits the leagues name and slug - from dashboard" do
     visit dashboard_path
     within('.' + @league.slug) do
       click_link "edit league name"
@@ -21,5 +21,19 @@ RSpec.describe "User can edit a league name" do
 
     expect(current_path).to eq(league_path(@league))
     expect(page).to have_content("Mark Miranda Super Dope League")
+  end
+
+  it "edits the leagues name and slug - from league show page" do
+    visit league_path(@league)
+    click_link("edit league name")
+
+    expect(current_path).to eq(edit_league_path(@league))
+    fill_in "League Name", with: "Second Test"
+    click_button "Submit League Changes"
+
+    @league.reload
+
+    expect(current_path).to eq(league_path(@league))
+    expect(page).to have_content("Second Test")
   end
 end
