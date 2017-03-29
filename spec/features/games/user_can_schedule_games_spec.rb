45deb14fb8b74b58f512c0ac9248c1790e2fcd75
Mark Miranda <notmarkmiranda@gmail.com>
2017-03-29 08:52:05 -0600

    finish other test

--- spec/features/games/user_can_schedule_games_spec.rb
+++ spec/features/games/user_can_schedule_games_spec.rb
@@ -23,5 +23,19 @@ RSpec.describe "User can schedule games", type: :feature do
     expect(page).to have_content("$100.00")
   end
 
-  it "from the league show page"
+  it "from the league show page" do
+    visit league_path(@league)
+    within(".league-actions") do
+      click_link "schedule game"
+    end
+
+    expect(current_path).to eq(new_league_game_path(@league))
+    fill_in "Date", with: "17 April 2016"
+    fill_in "Buy In", with: "50"
+    click_button "Schedule Game!"
+
+    expect(current_path).to eq(league_game_path(@league, Game.last))
+    expect(page).to have_content("April 17, 2016")
+    expect(page).to have_content("$50.00")
+  end
 end
