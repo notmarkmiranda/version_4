ac7514c26ecb3822464bfdbdfd63932439b61b6d
Mark Miranda <notmarkmiranda@gmail.com>
2017-05-25 10:55:10 -0600

    finished current season column

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,8 +1,30 @@
 class Season < ApplicationRecord
   belongs_to :league
   has_many :games
+  has_many :players, through: :games
+
+  def average_pot_size
+    total_pot_size / game_count.to_f
+  end
 
   def game_count
     games.count
+  end
+
+  def leader
+    season_participants = Participant.joins(:players).uniq
+    season_participants.max_by { |part| part.players.sum(&:raw_score) / part.players.count }
+  end
+
+  def player_count
+    players.count
+  end
+
+  def players_per_game
+    player_count / game_count.to_f
+  end
+
+  def total_pot_size
+    games.sum(&:actual_pot)
   end
 end
